86db739221a9e1acce79ca5265a63eac86127d71
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2018-07-22 21:09:22 +0100

    Moved to newDataset to enable method of testing previous dataset

--- ckd_svm.py
+++ ckd_svm.py
@@ -8,12 +8,13 @@ import messages as m
 DATASET_FILE='ckd_data.csv'
 OUTPUT_DIRECTORY='./OUTPUT/'
 
-dataset=l.loadDataset(DATASET_FILE)
-dataset=p.prepare(dataset)
-s.saveDataset(dataset)
-X_train,X_test,y_train,y_test,y=p.splitSets(dataset)
-s.saveSets(X_train,X_test,y_train,y_test,y)
-clf=c.createClf()
-s.saveClassifier(clf)
-c.trainClassifier(clf,X_train,y_train)
-m.printScoring(clf,X_test,y,y_test)
+def newDataset():
+    dataset=l.loadDataset(DATASET_FILE)
+    dataset=p.prepare(dataset)
+    s.saveDataset(dataset)
+    X_train,X_test,y_train,y_test,y=p.splitSets(dataset)
+    s.saveSets(X_train,X_test,y_train,y_test,y)
+    clf=c.createClf()
+    s.saveClassifier(clf)
+    c.trainClassifier(clf,X_train,y_train)
+    m.printScoring(clf,X_test,y,y_test)
