4e3c5a336da8c990e7ce1cbcdf8a5674a8732e80
Callum D E Smith <callumdesmith@callumdesmith.co.uk>
2018-07-22 20:04:15 +0100

    Prepare to normalise all fields to float value.

--- prepare.py
+++ prepare.py
@@ -5,7 +5,16 @@ def missingValuesToNan(dataset):
         dataset[col]=dataset[col].replace('?',numpy.nan)
     return dataset
 
+def createNominals(dataset):
+    NOM=dataset.loc[:,['sg','al','su','rbc','pc','pcc','ba','htn','dm','cad','appet','pe','ane','class']]
+    return NOM
+
+def createNumericals(dataset):
+    NUM=dataset.loc[:,['age','bp','bgr','bu','sc','sod','pot','hemo','pcv','wbcc','rbcc']]
+    return NUM
 
 def prepare(dataset):
     missingValuesToNan(dataset)
-    return
+    NOM=createNominals(dataset)
+    NUM=createNumericals(dataset)
+    return dataset
